3ddf5736175ca67b71dae481bf0aa4ceeebaef2b
Sheikh <kenneth.sheikh@hotmail.com>
2024-01-29 21:57:36 +0100

    all heroes attack and dragon counter, with some minor errors

--- script.js
+++ script.js
@@ -36,6 +36,8 @@ let dragonObject = {
 
 // Get the healer image element
 const healerImg = document.querySelector(".img-container.healer img");
+const archerImg = document.querySelector(".img-container.archer img");
+const warriorImg = document.querySelector(".img-container.warrior img");
 
 // Get elements for health bars and texts
 const dragonHealthBar = document.querySelector(".dragon-health");
@@ -64,30 +66,105 @@ function UpdateAllHealthBars() {
   SetHealthBar(heroesArray[2], warriorHealthBar);
 }
 
-// Add click event listener for the healer
-healerImg.addEventListener("click", function () {
-  // Handle Henriette Healer's action
-  if (heroesArray[0].alive && dragonObject.alive) {
+function attackDragon(hero) {
+  if (hero.alive && dragonObject.alive) {
     // Deal damage to the dragon
-    dragonObject.currentHP -= heroesArray[0].damage;
+    dragonObject.currentHP -= hero.damage;
     if (dragonObject.currentHP < 0) {
       dragonObject.currentHP = 0;
     }
 
-    // Limit healing to 100 HP and only heal alive heroes
-    heroesArray.forEach((hero) => {
-      if (hero.alive) {
-        const healAmount = Math.min(100, hero.maxHP - hero.currentHP);
-        hero.currentHP += healAmount;
-      }
-    });
+    // Display alert message
+    alert(`${hero.name} har gjort skade på ${dragonObject.name}`);
 
+    // Update health bars and texts
     UpdateAllHealthBars();
-
-    // Update health texts
     dragonHealthTxt.innerText = `${dragonObject.currentHP} / ${dragonObject.maxHP} HP`;
     healerHealthTxt.innerText = `${heroesArray[0].currentHP} / ${heroesArray[0].maxHP} HP`;
     archerHealthTxt.innerText = `${heroesArray[1].currentHP} / ${heroesArray[1].maxHP} HP`;
     warriorHealthTxt.innerText = `${heroesArray[2].currentHP} / ${heroesArray[2].maxHP} HP`;
   }
+}
+
+// Add click event listeners for each hero
+healerImg.addEventListener("click", function () {
+  attackDragon(heroesArray[0]);
+  performDragonCounterAttack();
 });
+
+archerImg.addEventListener("click", function () {
+  attackDragon(heroesArray[1]);
+  performDragonCounterAttack();
+});
+
+warriorImg.addEventListener("click", function () {
+  attackDragon(heroesArray[2]);
+  performDragonCounterAttack();
+});
+
+function updateHeroHealth(hero, healthBar, healthText) {
+  var life = hero.currentHP / hero.maxHP;
+  var pixels = life * 300;
+  healthBar.style.width = `${pixels}px`;
+  healthText.innerText = `${hero.currentHP} / ${hero.maxHP} HP`;
+}
+
+// Funksjon for å hente helsestolpen til en helt basert på ID
+function getHeroHealthBar(heroId) {
+  switch (heroId) {
+    case 0:
+      return healerHealthBar;
+    case 1:
+      return archerHealthBar;
+    case 2:
+      return warriorHealthBar;
+    default:
+      // Hvis ID ikke samsvarer med noen helt, returner null eller håndter på annen måte
+      return null;
+  }
+}
+
+// Funksjon for å hente helse-teksten til en helt basert på ID
+function getHeroHealthText(heroId) {
+  switch (heroId) {
+    case 0:
+      return healerHealthTxt;
+    case 1:
+      return archerHealthTxt;
+    case 2:
+      return warriorHealthTxt;
+    default:
+      // Hvis ID ikke samsvarer med noen helt, returner null eller håndter på annen måte
+      return null;
+  }
+}
+
+// Funksjon for å håndtere dragens motangrep
+function performDragonCounterAttack() {
+  console.log("Dragon counter attack!");
+  if (dragonObject.alive) {
+    if (Math.random() < 1 / 3) {
+      const randomHeroIndex = Math.floor(Math.random() * heroesArray.length);
+      const randomHero = heroesArray[randomHeroIndex];
+      console.log("Random Hero Index:", randomHeroIndex);
+      console.log("Random Hero:", randomHero);
+
+      if (randomHero.alive) {
+        // Reduser heltenes liv basert på dragens skade
+        randomHero.currentHP -= dragonObject.damage;
+
+        // Sjekk om helten er død
+        if (randomHero.currentHP <= 0) {
+          randomHero.currentHP = 0;
+          randomHero.alive = false;
+        }
+
+        updateHeroHealth(
+          randomHero,
+          getHeroHealthBar(randomHero.id),
+          getHeroHealthText(randomHero.id)
+        );
+      }
+    }
+  }
+}
